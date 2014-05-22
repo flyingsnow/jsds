8e8bd50f8e7b670c8564142578155cbc89f93f36
Ivan <flyingswin@gmail.com>
2014-05-22 20:49:48 +0800

    Update dict.js

--- dict.js
+++ dict.js
@@ -4,6 +4,8 @@ function Dictionary() {
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
 
 function add(key, value) {
@@ -19,7 +21,21 @@ function remove(key) {
 }
 
 function showAll() {
-    for each (var key in Object.keys(this.datastore)) {
+    for each (var key in Object.keys(this.datastore).sort()) {
         print(key + ' -> ' + this.datastore[key]);
+    }
+}
+
+function count() {
+    var n = 0;
+    for each (var key in Object.keys(this.datastore)) {
+        n++;
+    }
+    return n;
+}
+
+function clear() {
+    for each (var key in object.keys(this.datastore)) {
+        delete this.datastore[key];
     }
 }
