3b77a0198cdc8e4c3d5ce5d64b4a3e61562ed183
Ivan He <ivan@IvandeMacBook-Pro.local>
2016-07-15 07:37:31 +0800

    add a function

--- linkedlist.js
+++ linkedlist.js
@@ -5,5 +5,25 @@ function LinkedList() {
         this.next = null;
     };
 
+    var length = 0;
+    var head = null;
+
+    this.append = function(element) {
+        var node = new Node(element),
+            current;
+
+        if( head === null ) {
+            head = node;
+        } else {
+            current = head;
+            while( current.next ) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+        length++;
+    };
+
 
 }
