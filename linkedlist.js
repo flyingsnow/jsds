a6f724be9258fba8bf26144564c4c8bffe9f0daf
Ivan He <ivan@IvandeMacBook-Pro.local>
2016-07-19 07:27:42 +0800

    add removAt()

--- linkedlist.js
+++ linkedlist.js
@@ -19,11 +19,48 @@ function LinkedList() {
             while( current.next ) {
                 current = current.next;
             }
-
             current.next = node;
         }
         length++;
     };
 
+    this.removeAt(position) {
+        if( position > -1 && position < length ) {
+            var current = head,
+                previous,
+                index = 0;
 
+            if( position === 0 ) {
+                head = current.next;
+            }
+            else {
+                while( index++ < position ) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            length--;
+
+            return current.element;
+        }
+        else {
+            return null;
+        }
+    };
+
+    this.isEmpty = function() {
+        return length === 0;
+    }
+
+    this.getLength = function() {
+        return length;
+    }
 }
+
+var list = new LinkedList();
+list.append(15);
+list.append(10);
+
+alert("List length is " + list.getLength());
+alert("Remove " + list.removeAt(1) + "from list");
